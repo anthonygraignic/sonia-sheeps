2f8d2e4bdd4bf4cc8feb1a86146e38c844a81266
Anthony Graignic <anthony@graignic.com>
2022-11-24 13:38:43 +0100

    Init repo

---
+++ src/lib/components/ProposalResponse.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	export let from: string;
+	export let response: string;
+	export let wordsAmount: number;
+
+	// $: words =
+</script>
+
+<div>
+	<p>Résultat pour: <b>{from}</b> :</p>
+	<p>{response}</p>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import ProposalForm from '$lib/components/ProposalForm.svelte';
+	import ProposalResponse from '$lib/components/ProposalResponse.svelte';
+
+	function onResult(event: { detail: string }) {
+		console.log(event);
+		responses = [event.detail, ...responses];
+	}
+
+	let responses: string[] = [];
+</script>
+
+<main class="h-screen p-4">
+	<div class="flex flex-col items-center justify-items-center">
+		<h1 class="text-3xl font-bold mt-4 ">Les Moutons de Sonia</h1>
+		<p class="mt-2">
+			Et si au lieu de compter les moutons pour s'endormir, on se disait des mots en relation les un
+			avec les autres comme en exercice d'improvisation ?
+		</p>
+
+		<ProposalForm on:result={onResult} />
+
+		<hr class="my-8 h-px bg-gray-200 border-0 dark:bg-gray-700" />
+
+		<div class="grid grid-cols-1 divide-y gap-2 ">
+			{#each responses as response}
+				<ProposalResponse from={response.from} response={response.result} wordsAmount={2} />
+			{/each}
+		</div>
+	</div>
+</main>
